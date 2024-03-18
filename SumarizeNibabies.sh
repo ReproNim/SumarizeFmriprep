df81512d46a99334d32eb711f69ec92705997163
David Kennedy <David.Kennedy@umassmed.edu>
2024-03-18 17:39:39 -0400

    Update SumarizeNibabies.sh
    Rotations and signals (csf, white mater, and global signal) added

--- SumarizeNibabies.sh
+++ SumarizeNibabies.sh
@@ -24,7 +24,8 @@ else
 fi
 
 # Write output file header
-echo "Subject, Session, Task, Run, FD_AVG, FD_STD, XTrans_AVG, XTrans_STD, YTrans_AVG, YTrans_STD, ZTrans_AVG, ZTrans_STD" >> $outpt_file
+echo "Subject, Session, Task, Run, FD_AVG, FD_STD, XTrans_AVG, XTrans_STD, YTrans_AVG, YTrans_STD, ZTrans_AVG, ZTrans_STD, \
+	sXRot_AVG, XRot_STD, YRot_AVG, YRot_STD, ZRot_AVG, ZRot_STD, CSF_AVG, WhiteMatter_AVG, Global_AVG" >> $outpt_file
 
 # Scan nibabies directory for confounds files
 COUNTER=0
@@ -97,8 +98,81 @@ for f in ` find ${inpt_dir} -iname "*confounds_timeseries.tsv" `  ; do
 	#echo " FD std = $fdstd"
 	rm file1.txt
 	
+	# "rot_x"
+	col="rot_x"
+	awk -v column_val="$col" '{ if (NR==1) {val=-1; for(i=1;i<=NF;i++) { if ($i == column_val) {val=i;}}} if(val != -1) print $val} ' $f  > file1.txt
+	xravg=`awk 'NR>2 {s+=$1}END{print s/(NR-2)}' RS="\n"  file1.txt`
+	#echo "fdavg = $fdavg"
+	xrstd=$(
+	    tail -n +3 file1.txt |
+	        awk 'NR>2 {sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/(NR-1) - (sum/(NR-1))**2)}'
+	)
+	#echo " FD std = $fdstd"
+	rm file1.txt
+	
+	# "rot_y"
+	col="trans_y"
+	awk -v column_val="$col" '{ if (NR==1) {val=-1; for(i=1;i<=NF;i++) { if ($i == column_val) {val=i;}}} if(val != -1) print $val} ' $f  > file1.txt
+	yravg=`awk 'NR>2 {s+=$1}END{print s/(NR-2)}' RS="\n"  file1.txt`
+	#echo "fdavg = $fdavg"
+	yrstd=$(
+	    tail -n +3 file1.txt |
+	        awk 'NR>2 {sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/(NR-1) - (sum/(NR-1))**2)}'
+	)
+	#echo " FD std = $fdstd"
+	rm file1.txt
+	
+	# "rot_z"
+	col="rot_z"
+	awk -v column_val="$col" '{ if (NR==1) {val=-1; for(i=1;i<=NF;i++) { if ($i == column_val) {val=i;}}} if(val != -1) print $val} ' $f  > file1.txt
+	zravg=`awk 'NR>2 {s+=$1}END{print s/(NR-2)}' RS="\n"  file1.txt`
+	#echo "fdavg = $fdavg"
+	zrstd=$(
+	    tail -n +3 file1.txt |
+	        awk 'NR>2 {sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/(NR-1) - (sum/(NR-1))**2)}'
+	)
+	#echo " FD std = $fdstd"
+	rm file1.txt
+	
+	# "csf"
+	col="csf"
+	awk -v column_val="$col" '{ if (NR==1) {val=-1; for(i=1;i<=NF;i++) { if ($i == column_val) {val=i;}}} if(val != -1) print $val} ' $f  > file1.txt
+	csfavg=`awk 'NR>2 {s+=$1}END{print s/(NR-2)}' RS="\n"  file1.txt`
+	#echo "fdavg = $fdavg"
+	csfstd=$(
+	    tail -n +3 file1.txt |
+	        awk 'NR>2 {sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/(NR-1) - (sum/(NR-1))**2)}'
+	)
+	#echo " FD std = $fdstd"
+	rm file1.txt
+	
+	# "white_matter"
+	col="white_matter"
+	awk -v column_val="$col" '{ if (NR==1) {val=-1; for(i=1;i<=NF;i++) { if ($i == column_val) {val=i;}}} if(val != -1) print $val} ' $f  > file1.txt
+	wmavg=`awk 'NR>2 {s+=$1}END{print s/(NR-2)}' RS="\n"  file1.txt`
+	#echo "fdavg = $fdavg"
+	wmstd=$(
+	    tail -n +3 file1.txt |
+	        awk 'NR>2 {sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/(NR-1) - (sum/(NR-1))**2)}'
+	)
+	#echo " FD std = $fdstd"
+	rm file1.txt
+	
+	# "global_signal"
+	col="global_signal"
+	awk -v column_val="$col" '{ if (NR==1) {val=-1; for(i=1;i<=NF;i++) { if ($i == column_val) {val=i;}}} if(val != -1) print $val} ' $f  > file1.txt
+	gsavg=`awk 'NR>2 {s+=$1}END{print s/(NR-2)}' RS="\n"  file1.txt`
+	#echo "fdavg = $fdavg"
+	gsstd=$(
+	    tail -n +3 file1.txt |
+	        awk 'NR>2 {sum+=$1; sumsq+=$1*$1}END{print sqrt(sumsq/(NR-1) - (sum/(NR-1))**2)}'
+	)
+	#echo " FD std = $fdstd"
+	rm file1.txt
+	
 	# Write output file
-	echo "$subj, $sess, $task, $run, $fdavg, $fdstd, $xtavg, $xtstd, $ytavg, $ytstd, $ztavg, $ztstd" >> $outpt_file
+	echo "$subj, $sess, $task, $run, $fdavg, $fdstd, $xtavg, $xtstd, $ytavg, $ytstd, $ztavg, $ztstd, \
+		$xravg, $xrstd, $yravg, $yrstd, $zravg, $zrstd, $csfavg, $wmavg, $gsavg" >> $outpt_file
 	
 done
 
